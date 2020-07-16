212e525fa42ae273890679fbdb7770c28ab6429e
Jaret Hill <jaret.hill@tuta.io>
2020-07-16 10:14:53 -0500

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'gatsby';
+
+// import Image from '../components/image';
+import SEO from '../components/seo';
+import Header from '../components/header';
+import '../styles/styles.scss';
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <Header siteTitle="Site Title" />
+  </>
+);
+
+export default IndexPage;
